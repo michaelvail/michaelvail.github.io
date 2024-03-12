8f9340a2d868b47429175e233d08c3aad11d2543
Michael Vail <mvail24@g.holycross.edu>
2024-03-12 00:49:35 -0400

    Update authentication.js

--- authentication.js
+++ authentication.js
@@ -1,12 +1,12 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-button");
 const error = document.getElementById("error-message")
-const location = "https://michaelvail.github.io/success.html"
+const url = "https://michaelvail.github.io/success.html"
 
 loginButton.addEventListener("login", (e) => {
   e.preventDefault();
   if (document.getElementById("username") == "csci325" && document.getElementById("password") == "security") {
-    location.assign()
+    url.assign()
   }
   else {
     error.style.opacity = 1;
