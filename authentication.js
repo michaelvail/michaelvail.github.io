bef72246e9748a89177af2de0c6828ee1b51043e
Michael Vail <mvail24@g.holycross.edu>
2024-03-14 18:58:43 -0400

    auth

--- authentication.js
+++ authentication.js
@@ -3,12 +3,12 @@ const loginButton = document.getElementById("login-button");
 const error = document.getElementById("error-message")
 const url = "https://michaelvail.github.io/success.html"
 
-loginButton.addEventListener("click", (e) => {
-  e.preventDefault();
+function Authenticate()
+{
   if (document.getElementById("username").value === "csci325" && document.getElementById("password").value === "security") {
     window.location.href = url;
   }
   else {
     error.style.opacity = 1;
   }
-})
+}
